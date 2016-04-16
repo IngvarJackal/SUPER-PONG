b1932ad301ab590c36d7b273f90a4f68fa059dcd
ingvar <rz703a@gmail.com>
2016-04-16 08:52:10 +0300

    initial

---
+++ desktop/src/super/pong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package super.pong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import super.pong.SuperPongMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperPongMain(), config);
+	}
+}
